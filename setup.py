71c196aeef022b311dee27965def6e82350accbf
solovieff.nnov@gmail.com <solovieff.nnov@gmail.com>
2023-11-22 18:52:07 +0300

    version before the release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(HERE, "requirements.txt")) as f:
 
 setup(
     name="kibernikto",
-    version="1.0.12",
+    version="1.1",
     packages=find_packages(),
     install_requires=required,
     url='https://github.com/solovieff/kibernikto',
