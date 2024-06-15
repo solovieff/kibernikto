78550d23a16b1aa667ea065f0833d81042a7c129
asolovieff <solovieff.nnov@gmail.com>
2024-06-15 03:12:55 +0300

    default max words to 8500

--- kibernikto/bots/ai_settings.py
+++ kibernikto/bots/ai_settings.py
@@ -17,7 +17,7 @@ class AiSettings(BaseSettings):
     OPENAI_SUMMARY: str = """
     Give a short summary of the previous conversation in russian.
     """
-    OPENAI_MAX_WORDS: int = 5800
+    OPENAI_MAX_WORDS: int = 8500
     OPENAI_INPUT_PRICE: float | None = None
     OPENAI_OUTPUT_PRICE: float | None = None
 
