d01bdb886aa371b6dfc046b96c182ea2a0ee38af
asolovieff <solovieff.nnov@gmail.com>
2024-08-03 23:31:55 +0700

    Fix and update for bad groups default handlers and private replies access.

--- kibernikto/telegram/_default_handlers.py
+++ kibernikto/telegram/_default_handlers.py
@@ -17,10 +17,8 @@ async def private_message(message: types.Message):
     if not admin_or_public(message):
         negative_reply_text = f"Я не отвечаю на вопросы в личных беседах с незакомыми людьми (если это конечно не один из моиз Повелителей " \
                               f"снизошёл до меня). Я передам ваше соообщение мастеру."
-        await cd.tg_bot.send_message(user_id,
-                                     negative_reply_text)
-        await cd.tg_bot.send_message(cd.TELEGRAM_SETTINGS.TG_MASTER_IDS[0],
-                                     f"{message.from_user.username}: {message.md_text}")
+        await message.reply(text=negative_reply_text)
+        await message.forward(cd.TELEGRAM_SETTINGS.TG_MASTER_ID)
     else:
         # TODO: plugins should be reworked and combined with preprocessor
         user_text = await cd.preprocessor.process_tg_message(message,
@@ -49,37 +47,37 @@ async def private_message(message: types.Message):
     or_f(F.chat.type == enums.ChatType.GROUP, F.chat.type == enums.ChatType.SUPERGROUP))
 async def group_message(message: types.Message):
     chat_id = message.chat.id
-    if cd.TELEGRAM_SETTINGS.TG_FRIEND_GROUP_IDS and chat_id not in cd.TELEGRAM_SETTINGS.TG_FRIEND_GROUP_IDS:
-        negative_reply_text = (f"Я не общаюсь в беседах, в которых мне не велено участвовать"
-                               f" (если это конечно не один из моих Повелителей"
-                               f" снизошёл до меня). Я передам ваше соообщение кому-нибудь.")
-        print(f"allowed chats: {cd.TELEGRAM_SETTINGS.TG_FRIEND_GROUP_IDS}, given chat: {chat_id}")
-        await cd.tg_bot.send_message(chat_id,
-                                     negative_reply_text)
-        await cd.tg_bot.send_message(cd.TELEGRAM_SETTINGS.TG_MASTER_IDS[0],
-                                     f"{message.from_user.username}: {message.md_text}")
+
+    if not executor_exists(chat_id):
+        # loading full chat info for the first time
+        chat_info: Chat = await cd.tg_bot.get_chat(chat_id)
     else:
-        if not executor_exists(chat_id):
-            # loading full chat info for the first time
-            chat_info: Chat = await cd.tg_bot.get_chat(chat_id)
-        else:
-            chat_info = message.chat
-        group_ai = get_ai_executor_full(chat=chat_info)
+        chat_info = message.chat
+    group_ai = get_ai_executor_full(chat=chat_info)
 
-        if cd.is_reply(message) or group_ai.should_react(message.html_text):
-            user_text = await cd.preprocessor.process_tg_message(message,
-                                                                 tg_bot=cd.tg_bot)
-            if user_text is None:
-                return None  # do not reply
+    if cd.is_reply(message) or group_ai.should_react(message.html_text):
+        if cd.TELEGRAM_SETTINGS.TG_FRIEND_GROUP_IDS and chat_id not in cd.TELEGRAM_SETTINGS.TG_FRIEND_GROUP_IDS:
+            negative_reply_text = (f"Я не общаюсь в беседах, в которых мне не велено участвовать"
+                                   f" (если это конечно не один из моих Повелителей"
+                                   f" снизошёл до меня). Я передам ваше соообщение кому-нибудь.")
+            print(f"allowed chats: {cd.TELEGRAM_SETTINGS.TG_FRIEND_GROUP_IDS}, given chat: {chat_id}")
+            await message.reply(text=negative_reply_text)
+            await message.forward(chat_id=cd.TELEGRAM_SETTINGS.TG_MASTER_ID)
+            return None
 
-            await cd.tg_bot.send_chat_action(message.chat.id, 'typing')
-            reply_text = await group_ai.heed_and_reply(message=user_text,
-                                                       author=f"{chat_id}_{message.from_user.username}")
+        user_text = await cd.preprocessor.process_tg_message(message,
+                                                             tg_bot=cd.tg_bot)
+        if user_text is None:
+            return None  # do not reply
 
-            chunks = split_text_by_sentences(reply_text,
-                                             cd.TELEGRAM_SETTINGS.TG_MAX_MESSAGE_LENGTH)
-            for chunk in chunks:
-                await message.reply(text=chunk)
+        await cd.tg_bot.send_chat_action(message.chat.id, 'typing')
+        reply_text = await group_ai.heed_and_reply(message=user_text,
+                                                   author=f"{chat_id}_{message.from_user.username}")
+
+        chunks = split_text_by_sentences(reply_text,
+                                         cd.TELEGRAM_SETTINGS.TG_MAX_MESSAGE_LENGTH)
+        for chunk in chunks:
+            await message.reply(text=chunk)
 
 
 def imported_ok():
