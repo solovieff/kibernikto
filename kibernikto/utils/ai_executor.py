2de961da69d1ea74988e7554ec26fb275dde62f1
asolovieff <solovieff.nnov@gmail.com>
2024-08-04 18:07:55 +0700

    important fix

--- kibernikto/utils/ai_executor.py
+++ kibernikto/utils/ai_executor.py
@@ -1,6 +1,5 @@
 from aiogram.types import Message, Chat
 
-from avatar.bots.ragger import Kiberagkto
 from kibernikto.telegram import executor_exists, get_ai_executor_full
 
 
@@ -11,5 +10,5 @@ async def get_ready_executor(message: Message):
         chat_info: Chat = await message.bot.get_chat(chat_id)
     else:
         chat_info = message.chat
-    user_ai: Kiberagkto = get_ai_executor_full(chat=chat_info, user=message.from_user)
+    user_ai = get_ai_executor_full(chat=chat_info, user=message.from_user)
     return user_ai
