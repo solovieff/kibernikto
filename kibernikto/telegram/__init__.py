2df23770aa558d24776cbbb84ad5feb80c02e1b4
asolovieff <solovieff.nnov@gmail.com>
2024-11-16 19:47:13 +0300

    some methods

--- kibernikto/telegram/__init__.py
+++ kibernikto/telegram/__init__.py
@@ -1,1 +1,1 @@
-from ._executor_corral import get_ai_executor, executor_exists, get_ai_executor_full, kill
+from ._executor_corral import init, get_ai_executor, executor_exists, get_ai_executor_full, kill, get_temp_executor
