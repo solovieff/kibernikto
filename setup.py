b5ec12d59e66874c5b82bd93e6da93fee88955c6
asolovieff <solovieff.nnov@gmail.com>
2025-05-23 11:47:57 +0300

    pytubefix back to == 8.13.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(HERE, "requirements.txt")) as f:
 
 setup(
     name="kibernikto",
-    version="1.5.2",
+    version="1.5.3",
     packages=find_packages(),
     install_requires=required,
     url='https://github.com/solovieff/kibernikto',
